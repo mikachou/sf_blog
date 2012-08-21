5d9c4ede3689db46900c5995fbf71d00297d1fcd
Michaël <michael@5732Z.(none)>
2012-08-21 21:55:18 +0200

    création BlogBundle + routes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Symfony\Bundle\DoctrineMongoDBBundle\DoctrineMongoDBBundle(),
+            new Schuh\BlogBundle\SchuhBlogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
